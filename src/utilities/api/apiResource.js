5c7e4a415d39cc9d6f611bb57255578c38c6f379
MinnHtetZaw <minnhtetzaw.dev@gmail.com>
2023-08-30 11:51:35 +0630

    master

--- src/utilities/api/apiResource.js
+++ src/utilities/api/apiResource.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api= axios.create({
     // baseURL : 'http://192.168.100.5:8000/api'
-    baseURL : 'http://192.168.1.20:8000/api'
+    // baseURL : 'http://192.168.1.20:8000/api'
     // baseURL : 'http://127.0.0.1:8000/api'
-
+    baseURL: 'http://kwinengineeringbackend.kwintechnologies.com/api'
 })
